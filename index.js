1120bdc6dd10ca7a1aee4b1a08d308893b73a75a
MD Arif <arifreza974@gmail.com>
2023-06-19 18:42:52 +0530

    added edit function

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 let inputValue = document.querySelector(".todo-input");
 let addBtn = document.querySelector("#addBtn");
 let todoList = document.querySelector(".todo-list");
+let todoListCnt = document.querySelector(".todo-list-container");
 
+let clickLI = "";
+console.log(typeof clickLI);
 function addTask() {
-    let val = inputValue.value;
+  let val = inputValue.value;
   if (val === "") {
     alert("Please write something");
   } else {
-    let liElem = document.createElement("li");
-    liElem.innerHTML = val;
-    todoList.appendChild(liElem);
+    if (typeof clickLI === "object") {
+      clickLI.innerText = val;
 
-    imgElem = document.createElement("img");
-    imgElem.setAttribute("src", "delete.png");
-    imgElem.setAttribute("alt", "Delete button");
+      imgElem = document.createElement("img");
+      imgElem.setAttribute("src", "delete.png");
+      imgElem.setAttribute("alt", "Delete button");
 
-    liElem.appendChild(imgElem);
+      clickLI.appendChild(imgElem);
+
+      let editElem = document.createElement("button");
+      editElem.setAttribute("class", "editBtn");
+      editElem.innerText = "Edit";
+      clickLI.appendChild(editElem);
+      addBtn.innerText = "Add";
+
+    } else {
+      console.log(clickLI);
+      let liElem = document.createElement("li");
+      liElem.innerHTML = val;
+      todoList.appendChild(liElem);
+
+      imgElem = document.createElement("img");
+      imgElem.setAttribute("src", "delete.png");
+      imgElem.setAttribute("alt", "Delete button");
+
+      liElem.appendChild(imgElem);
+
+      let editElem = document.createElement("button");
+      editElem.setAttribute("class", "editBtn");
+      editElem.innerText = "Edit";
+      liElem.appendChild(editElem);
+    }
+
     inputValue.value = "";
     saveData();
   }
@@ -23,15 +50,16 @@ function addTask() {
 
 addBtn.addEventListener("click", addTask);
 
-
 todoList.addEventListener("click", function (e) {
-    if (e.target.tagName === "IMG") {
-        e.target.parentElement.remove();
-        saveData();
-        
-    }
-    });
-
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    saveData();
+  } else if (e.target.tagName === "BUTTON") {
+    inputValue.value = e.target.parentNode.innerText;
+    addBtn.innerText = "Edit";
+    clickLI = e.target.parentElement;
+  }
+});
 
 function saveData() {
   localStorage.setItem("data", todoList.innerHTML);
